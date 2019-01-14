5d283be72e5dc37d2757fdc8cd2539d8cf918166
Anthony Rolfe <arolfe@grantham.edu>
2019-01-13 20:09:24 -0700

    update to css, html and django framework settings.

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    "/home/hitman1978/Documents/CodingProjects/Programming Projects/Django/blog_website/blog/static/",
+]
