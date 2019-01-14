e72c23252b6f1be7fe1546456ea73d8f86d4fd1b
Anthony Rolfe <arolfe@grantham.edu>
2019-01-13 18:49:40 -0700

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+"""
+This file handles the url patterns for displaying the blog as opposed to displaying the
+admin side of the web_site.
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
